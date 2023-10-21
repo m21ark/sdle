186d2da43ba4432ec02f4d2f2b070fe916acda7a
Ricardo de Matos <ricardo.andre.de.matos@gmail.com>
2023-10-21 19:17:35 +0100

    changes to the commit list

--- frontend/logic/shopping_list.js
+++ frontend/logic/shopping_list.js
@@ -40,18 +40,41 @@ class ShoppingList {
         }
     }
 
-    commitChanges() {
+    serialize() {
+        // Serialize the shopping list 
+        return JSON.stringify(Array.from(this.products));
+    }
+
+    deserialize(serialized) {
+        // Deserialize the shopping list
+        //this.products = new Map(JSON.parse(serialized));
+        //
+        // TODO: deserialize the shopping list ... counter etc
+        //
+    }
+
+    changesAfter(commitHash) {
+        // Return a new ShoppingList with the changes after the given commit hash
+        const newShoppingList = new ShoppingList();
+        for (const [hash, shoppingList] of this.commits) {
+            if (hash > commitHash) {
+                newShoppingList.merge(shoppingList);
+            }
+        }
+        return newShoppingList;
+    }
+
+
+    commitChanges(commitHash) {
         // Commit changes to the shopping list
-        const commitHash = this.commitHash();
         this.commits.set(commitHash, this);
-        console.log('Commit hash:', commitHash);
-        console.log('Commit:', this.commits.get(commitHash));
-        this.commits.get(commitHash).showList();
-        return commitHash;
+        localStorage.setItem('shoppingList', JSON.stringify(Array.from(this.commits)));
+        return commitHash; // TODO: commit changes should be in cache and be serialized back .... still need to do the serialization part
     }
 
     merge(other) {
         // Merge product counters from another ShoppingList
+        const commitHash = this.commitHash();
         for (const [productName, counter] of other.products) {
             if (!this.products.has(productName)) {
                 this.products.set(productName, new PNCounter());
@@ -59,6 +82,8 @@ class ShoppingList {
             const productCounter = this.products.get(productName);
             productCounter.join(counter);
         }
+        this.commitChanges(commitHash);
+        other.commitChanges(commitHash);
     }
 }
 
@@ -67,27 +92,29 @@ class ShoppingList {
 // Example usage:
 
 
-const replica1 = new ShoppingList();
-const replica2 = new ShoppingList();
-
-replica1.addProduct('Apples', 3);
-replica1.addProduct('Bananas', 2);
-
-replica2.addProduct('Bananas', 1);
-replica2.removeProduct('Apples', 2);
-
-// Merge the two replicas
-replica1.merge(replica2);
-replica2.merge(replica1);
-
-console.log('Replica 1:');
-replica1.showList();
-console.log('Replica 2:');
-replica2.showList();
 
 
-// Commit changes to the shopping list
-const commitHash1 = replica1.commitChanges();
-
-replica1.addProduct('Bananas', 10);
-
+// const replica1 = new ShoppingList();
+// const replica2 = new ShoppingList();
+// 
+// replica1.addProduct('Apples', 3);
+// replica1.addProduct('Bananas', 2);
+// 
+// replica2.addProduct('Bananas', 1);
+// replica2.removeProduct('Apples', 2);
+// 
+// // Merge the two replicas
+// replica1.merge(replica2);
+// replica2.merge(replica1);
+// 
+// console.log('Replica 1:');
+// replica1.showList();
+// console.log('Replica 2:');
+// replica2.showList();
+// 
+// // Commit changes to the shopping list
+// const commitHash1 = replica1.commitChanges();
+// 
+// replica1.addProduct('Bananas', 10);
+// 
+// 
