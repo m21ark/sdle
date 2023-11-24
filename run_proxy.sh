8b2a1afad2f9149c4c364be0cd0b8049e0012725
Johnny-droid <joaoricardoralves@gmail.com>
2023-11-24 09:40:10 +0000

    Added Functions to proxy and script to run

---
+++ run_proxy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: <port> <number_of_instances>"
+  exit 1
+fi
+
+base_port=$1
+num_instances=$2
+
+echo "Starting $num_instances instances of proxy on ports $base_port to $((base_port + num_instances - 1))"
+
+i=0
+while [ "$i" -lt "$num_instances" ]; do
+  port=$((base_port + i))
+
+  npx nodemon ./connection/proxy.js $port
+
+  i=$((i + 1))
+done
+
+# Wait for all background processes to finish
+wait
