909c7752c3d55b529c64f151bcb0181e8851c93d
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2023-12-02 20:52:18 +0000

    temporary solution

--- run_everything.sh
+++ run_everything.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
 
+# Function to restart a replica
+restart_replica() {
+    local replica_number=$1
+    local port=$((5000 + replica_number))
+    echo "Restarting replica $replica_number on port $port"
+
+    # Add your command to restart the replica here
+    npx nodemon ./db/index.js $port 2>&1 &
+}
+
+
 # Run replicas in the background
 sh ./run_backend.sh 5 &
 sh ./run_replicas.sh 5 &
@@ -9,8 +20,34 @@ sh ./run_proxy.sh 4000 1 &
 
 sleep 10
 
-# start random killing
-sh ./kill_random_replicas.sh &
+# Run indefinitely
+while true; do
+    # Generate a random number between 1 and 5
+    random_replica=$(shuf -i 0-4 -n 1)
+    echo "Stopping a random replica: $random_replica"
+
+    port_number=$((random_replica + 5000))
+
+    # Find the process ID (PID) using the specified port
+    pid=$(lsof -t -i :$port_number)
+    
+    # Check if a process was found
+    if [ -n "$pid" ]; then
+        # Kill the process
+        kill $pid
+        echo "Process on port $port_number killed successfully."
+    else
+        echo "No process found on port $port_number."
+    fi
+
+    # Wait for some time (e.g., 10 seconds) before restarting the replica
+    sleep 10
+
+    # Restart the stopped replica
+    restart_replica $random_replica
+
+    sleep 10
+done
 
 
 
