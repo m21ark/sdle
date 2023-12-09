d18db8300e70443457a5b4ec3f60910ac9ff1674
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2023-12-09 16:49:42 +0000

    garbage collection

--- db/garbage_collection.js
+++ db/garbage_collection.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const { parse } = require("path");
 
 class GarbageCollector {
   constructor() {
@@ -49,7 +50,7 @@ class GarbageCollector {
     const replicasCommits = new Map();
 
     for (const port of this.replicaPorts) {
-      const response = await fetch(`http://127.0.0.1:${port}/list/${listName}`);
+      const response = await fetch(`http://127.0.0.1:${port}/list/${encodeURIComponent(listName)}`);
 
       if (response.status === 200) {
         const data = await response.json();
@@ -110,6 +111,9 @@ class GarbageCollector {
     const totalLists = listNames.length;
 
     let copy_of_port = [... this.replicaPorts];
+    copy_of_port.push(copy_of_port[0])
+    copy_of_port.push(copy_of_port[1])
+
     for (let i = 0; i < copy_of_port.length - 2; i += 1) {
       
       this.replicaPorts = copy_of_port.slice(i, i + batchSize);
@@ -130,6 +134,8 @@ class GarbageCollector {
 
     // replicasCommits is a Map of the form: PORT => [ {commitHash: ..., commitData: ...} ]
     const replicasCommits = await this.getReplicasCommits(listName);
+
+    console.log("Found commits on replicas:", replicasCommits);
 
     // Sort commits by commit_hash
     const sortedReplicasCommits = new Map(
@@ -184,18 +190,18 @@ class GarbageCollector {
     const mergedCommitData = Array.from(commonCommitDataMap.values()).reduce(
       (merged, commitData) => {
         const commitDataObj = JSON.parse(
-          commitData.replace(/delta/g, '"delta"')
+          commitData.replace(/delta/g, 'delta')
         );
 
         for (const key in commitDataObj.delta) {
           if (merged.delta.hasOwnProperty(key))
-            merged.delta[key] += commitDataObj.delta[key];
-          else merged.delta[key] = commitDataObj.delta[key];
+            merged.delta[key] += parseInt(commitDataObj.delta[key]);
+          else merged.delta[key] = parseInt(commitDataObj.delta[key]);
         }
 
         return merged;
       },
-      { delta: {} }
+      { "delta": {} }
     );
 
     const lastCommonCommitHash = sortedReplicasCommits
@@ -220,6 +226,7 @@ class GarbageCollector {
   async sendMergedCommitToReplicas(listName, common_hashes, mergedCommit) {
     console.log("Sending merged commit to replicas: ", mergedCommit);
 
+    console.log("Replicas to send to:", JSON.stringify(mergedCommit).replace(/delta/g, '"delta"'));
     const sendRequests = this.replicaPorts.map(async (port) => {
       try {
         const response = await fetch(
@@ -229,16 +236,16 @@ class GarbageCollector {
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
               common_hashes: common_hashes,
-              data: mergedCommit,
+              data: mergedCommit
             }),
           }
         );
 
-        const data = await response.json();
+        // const data = await response.json();
 
-        if (response.status !== 200) {
-          console.log("Error sending merged commit to replica:", data);
-        }
+        // if (response.status !== 200) {
+          // console.log("Error sending merged commit to replica:", data);
+        // }
       } catch (error) {
         console.error(error);
       }
@@ -251,17 +258,4 @@ class GarbageCollector {
 const garbageCollector = new GarbageCollector();
 garbageCollector.performGarbageCollection(); // TODO Make this a cron job
 
-// using this endpoint, get all commits for a list
 
-/*
-app.get("/list/:list_name", (req, res) => {
-  const listName = req.params.list_name;
-
-  let response = queryAll(
-    "SELECT commit_hash, commit_data FROM commitChanges WHERE list_name = ?",
-    [listName]
-  );
-
-  res.status(200).json(response);
-});
-*/
